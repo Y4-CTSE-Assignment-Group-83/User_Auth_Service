504c55eb8fb4b02d78d77c6bbef2e3dd9c45dd9e
Janindu Chamod <132637475+Janinduchamod2001425@users.noreply.github.com>
2026-03-16 20:44:30 +0530

    Update staff.validators.js

--- backend/src/validators/staff.validators.js
+++ backend/src/validators/staff.validators.js
@@ -24,6 +24,6 @@ export const updateStaffProfileValidation = [
 
   body("gender")
     .optional()
-    .isIn(["MALE", "FEMALE", "OTHER"])
-    .withMessage("Gender must be MALE, FEMALE, or OTHER"),
+    .isIn(["male", "female", "other"])
+    .withMessage("Gender must be male, female, or other"),
 ];
