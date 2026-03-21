de8769eac68d5f1a11a4d6a5422a0a026fe35c59
Janindu Chamod <132637475+Janinduchamod2001425@users.noreply.github.com>
2026-03-21 12:03:57 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -34,7 +34,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000", "http://localhost:4000"],
     credentials: true,
   }),
 );
