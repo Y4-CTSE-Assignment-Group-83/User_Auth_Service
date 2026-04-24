79e96795560275abb4bf6cb024b4e5b6a7d2ece9
Janindu Chamod <132637475+Janinduchamod2001425@users.noreply.github.com>
2026-04-24 18:33:03 +0530

    Updated

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -4,18 +4,18 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const connectDB = async () => {
-    try {
+  try {
+    // if (!process.env.MONGO_URI) {
+    //   throw new Error("MONGO_URI is not defined in environment variables");
+    // }
 
-        if (!process.env.MONGO_URI) {
-            throw new Error("MONGO_URI is not defined in environment variables");
-        }
+    const conn = await mongoose.connect(
+      "mongodb+srv://janiduchamod25_db_user:123@auth.qsy77xu.mongodb.net/auth?appName=auth",
+    );
 
-        const conn = await mongoose.connect(process.env.MONGO_URI);
-
-        console.log(`MongoDB Connected: ${conn.connection.host}`);
-
-    } catch (error) {
-        console.error("MongoDB Connection Failed:", error.message);
-        process.exit(1);
-    }
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error("MongoDB Connection Failed:", error.message);
+    process.exit(1);
+  }
 };
